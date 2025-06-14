a7d1ed0516fbea1100d1ff4b6fa37ee1f367bee5
Dasara Thanmai sri sathvika <sathvikadasara06@gmail.com>
2025-06-14 12:27:23 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a=8;
+char ch='c';
+float fl=8.06;
+printf("this is a integer %d\n",a);
+printf("this is a character %c\n", ch);
+printf("this is a floting ppoint number %f\n", fl);
+
+return 0;
+}
