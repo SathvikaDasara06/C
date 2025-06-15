97f018fa673cf15fea7b4b34fc62316b52e941e3
Dasara Thanmai sri sathvika <sathvikadasara06@gmail.com>
2025-06-15 19:10:00 +0530

    Add files via upload

---
+++ pr11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int a,b,area;
+    printf("length of rectangle:");
+    scanf("%d", &a);
+    printf("breadth of rectangle:");
+    scanf("%d", &b);
+    area=a*b;
+    printf("area of rectangle is %d", area);
+    return 0;
+
+}
