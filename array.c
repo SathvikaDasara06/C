4bbc013fab980b1db68ab844d64689a62a79f3f7
Dasara Thanmai sri sathvika <sathvikadasara06@gmail.com>
2025-06-19 15:33:03 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a,b,i,j,sum;
+    
+    printf("enter the array a:");
+    scanf("%d",&a);
+    printf("enter the array b:");
+    scanf("%d",&b);
+    
+    sum=(a+b);
+    printf("the sum of the two numbers is %d", sum);
+    
+    return 0;
+}
